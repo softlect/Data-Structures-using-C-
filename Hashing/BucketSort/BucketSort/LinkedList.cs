6353464a6c3d1fd6f3d5c164b8508b21a689f407
softlect <64558769+softlect@users.noreply.github.com>
2020-10-08 20:45:29 +0530

    Add files via upload

---
+++ Hashing/BucketSort/BucketSort/LinkedList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BucketSort
+{
+    class Node
+    {
+        public int element;
+        public Node next;
+        public Node(int e, Node n)
+        {
+            element = e;
+            next = n;
+        }
+    }
+
+    class LinkedList
+    {
+        Node head;
+        Node tail;
+        int size;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void addLast(int e)
+        {
+            Node newest = new Node(e, null);
+            if (isEmpty())
+                head = newest;
+            else
+                tail.next = newest;
+            tail = newest;
+            size = size + 1;
+        }
+
+        public void addFirst(int e)
+        {
+            Node newest = new Node(e, null);
+            if (isEmpty())
+            {
+                head = newest;
+                tail = newest;
+            }
+            else
+            {
+                newest.next = head;
+                head = newest;
+            }
+            size = size + 1;
+        }
+
+        public void addAny(int e, int position)
+        {
+            Node newest = new Node(e, null);
+            Node p = head;
+            int i = 1;
+            while (i < position - 1)
+            {
+                p = p.next;
+                i = i + 1;
+            }
+            newest.next = p.next;
+            p.next = newest;
+            size = size + 1;
+        }
+
+        public int removeFirst()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return -1;
+            }
+            int e = head.element;
+            head = head.next;
+            size = size - 1;
+            if (isEmpty())
+                tail = null;
+            return e;
+        }
+
+        public int removeLast()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("List is Empty");
+                return -1;
+            }
+            Node p = head;
+            int i = 1;
+            while (i < size - 1)
+            {
+                p = p.next;
+                i = i + 1;
+            }
+            tail = p;
+            p = p.next;
+            int e = p.element;
+            tail.next = null;
+            size = size - 1;
+            return e;
+        }
+
+        public int removeAny(int position)
+        {
+            Node p = head;
+            int i = 1;
+            while (i < position - 1)
+            {
+                p = p.next;
+                i = i + 1;
+            }
+            int e = p.next.element;
+            p.next = p.next.next;
+            size = size - 1;
+            return e;
+        }
+
+        public void display()
+        {
+            Node p = head;
+            while (p != null)
+            {
+                Console.Write(p.element + " --> ");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Hashing/BucketSort/BucketSort/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BucketSort
+{
+    class Program
+    {
+        LinkedList[] buckets;
+
+        public Program()
+        {
+            buckets = new LinkedList[10];
+            for (int i = 0; i < 10; i++)
+            {
+                buckets[i] = new LinkedList();
+            }
+
+        }
+
+        public void bucketsort(int[] A, int n)
+        {
+            int maxelement = max(A,n);
+            for(int i=0;i<n;i++) 
+            {
+                int j = (int)(n * A[i] / (maxelement + 1));
+                buckets[j].addLast(A[i]);
+            }
+            for(int i=0;i<10;i++) 
+            {
+                int x = buckets[i].length();
+                int[] temp = new int[x];
+                for(int j=0;j<x;j++)
+                    temp[j] = buckets[i].removeFirst();
+                insertionsort(temp,x);
+                for(int j=0;j<x;j++)
+                    buckets[i].addLast(temp[j]);
+            }
+            int k = 0;
+            for(int i=0;i<10;i++) 
+            {
+                int m = buckets[i].length();
+                for (int j = 0; j < m; j++) 
+                {
+                    A[k] = buckets[i].removeFirst();
+                    k = k + 1;
+                }
+            }
+        }
+
+        public int max(int[] A, int n) 
+        {
+            int temp = 0;
+            for(int i=0;i<n;i++)
+                if (A[i]>temp)
+                    temp = A[i];
+            return temp;
+        }
+
+        public void insertionsort(int[] A, int n) 
+        {
+            for(int i=1;i<n;i++) 
+            {
+                int temp = A[i];
+                int position = i;
+                while(position>0 && A[position-1] > temp) 
+                {
+                    A[position] = A[position-1];
+                    position = position - 1;
+                }
+                A[position] = temp;
+            }
+        }
+
+        public void display(int[] A, int n) 
+        {
+            for(int i=0;i<n;i++)
+                Console.Write(A[i] + " ");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Program s = new Program();
+            int[] A = {54, 78, 64, 92, 34, 86, 28};
+            Console.WriteLine("Original Array: ");
+            s.display(A, 7);
+            s.bucketsort(A, 7);
+            Console.WriteLine("Sorted Array: ");
+            s.display(A, 7);
+            Console.ReadKey();
+        }
+    }
+}
